315a9acc050f5836372757a1807c322bda65be73
tlofresh <longtm@gmail.com>
2018-05-15 10:25:20 -0400

    Updated contact sensor state check.

--- devicetypes/tmlong/vthing-contact-sensor.src/vthing-contact-sensor.groovy
+++ devicetypes/tmlong/vthing-contact-sensor.src/vthing-contact-sensor.groovy
@@ -15,49 +15,29 @@
  */
 metadata {
     definition (name: "vThing Contact Sensor", namespace: "tmlong", author: "Todd Long") {
-        capability "Battery"
         capability "Contact Sensor"
         capability "Refresh"
         capability "Sensor"
-        capability "Temperature Measurement"
     }
 
     simulator {
-        // TODO: define status and reply messages here
     }
 
     tiles(scale: 2) {
         multiAttributeTile(name: "contact", type: "generic", width: 6, height: 4) {
             tileAttribute("device.contact", key: "PRIMARY_CONTROL") {
-                attributeState "open", label: '${name}', icon: "st.contact.contact.open", backgroundColor: "#00A0DC"
-                attributeState "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#ffffff"
+                attributeState "open", label: '${name}', icon: "st.contact.contact.open", backgroundColor: "#e86d13"
+                attributeState "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#00A0DC"
                 attributeState "someOpen", label: "some open", icon: "st.contact.contact.closed", backgroundColor: "#79b821"
             }
-        }
-
-        valueTile("temperature", "device.temperature", inactiveLabel: false, width: 2, height: 2) {
-            state "temperature", label: '${currentValue}°',
-                    backgroundColors: [
-                            [value: 31, color: "#153591"],
-                            [value: 44, color: "#1e9cbb"],
-                            [value: 59, color: "#90d2a7"],
-                            [value: 74, color: "#44b621"],
-                            [value: 84, color: "#f1d801"],
-                            [value: 95, color: "#d04e00"],
-                            [value: 96, color: "#bc2323"]
-                    ]
-        }
-
-        valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
-            state "battery", label: '${currentValue}% battery', unit: ""
         }
 
         standardTile("refresh", "device.contact", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
             state "default", action: "refresh.refresh", icon: "st.secondary.refresh"
         }
 
-        main (["contact", "temperature"])
-        details(["contact", "temperature", "battery", "refresh"])
+        main "contact"
+        details(["contact", "refresh"])
     }
 }
 
@@ -72,13 +52,13 @@ def determineState(contacts) {
     log.debug "determineState() contacts: ${contacts}"
 
     // determine which contacts are open
-    def contactsOpen = contacts.count { it.currentContact == _ContactState.ON }
+    def contactsOpen = contacts.count { it.currentContact == _ContactState.OPEN }
 
     // determine the contact state
-    def contactState = contactsOpen == contacts.size() ? _ContactState.ON
-        : (!contactsOpen ? _ContactState.OFF : _ContactState.SOME)
+    def contactState = contactsOpen == contacts.size() ? _ContactState.OPEN
+        : (!contactsOpen ? _ContactState.CLOSED : _ContactState.SOME)
 
-    log.debug "determineState() contactState: ${contactState}"
+    log.debug "determineState() contactsOpen: ${contactsOpen} contactState: ${contactState}"
 
     contactState
 }
